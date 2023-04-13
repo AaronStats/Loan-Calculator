a145811f61811d3392281119e1184400048eb69e
Aaron Statler <aaron-statler@uiowa.edu>
2023-04-13 12:06:50 -0500

    .

--- 404.py
+++ 404.py
@@ -1,14 +1,8 @@
 import pytest
 from app import app, Loan
 
-
 def test_full_loan_calculation(client):
-    """
-    GIVEN a user enters their loan details
-    WHEN the user clicks the calculate button
-    THEN the user sees the monthly payment for their loan and the amortization table
-    """
     response = client.GET(
-        "/astatler", data={"loanAmt": "100000", "lengthOfLoan": "30", "intRate": "0.06"}
+        "/astatler"
     )
     assert response.status_code == 404
